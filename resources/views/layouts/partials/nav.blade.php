7e3559be571ab6f94c6c2a2559af5d34355879cd
sant1dom <santone.domenico2k@gmail.com>
2020-12-29 09:52:06 +0100

    Aggiustata navbar in modo da farla rimanere fissata in alto

--- resources/views/layouts/partials/nav.blade.php
+++ resources/views/layouts/partials/nav.blade.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light ">
+<nav class="navbar fixed-top navbar-expand-lg navbar-light bg-light ">
     <a class="navbar-brand my-auto" href="/">Hotel Name</a>
 
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
